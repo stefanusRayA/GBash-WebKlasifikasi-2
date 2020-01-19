92d1a4c5094d72a3a9929898975481be17648cc8
Stefanus Ray A <45416814+stefanusRayA@users.noreply.github.com>
2020-01-19 20:32:54 +0700

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,6 +8,7 @@
   <meta name="author" content="">
 
   <title>SIDIKLILA | Sistem Prediksi Lirik Lagu</title>
+    <link rel = "icon" href ="/ML/img/stefanus.jpg" type = "image/x-icon">
 
   <!-- Bootstrap Core CSS -->
   <link href="/ML/css/bootstrap.min.css" rel="stylesheet" type="text/css">
